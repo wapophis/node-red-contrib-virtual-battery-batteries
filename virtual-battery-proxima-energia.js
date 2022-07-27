6c93e1d566d597d791ca82f03501f7c2a18441bd
Wapophis <luis.martinez.exts@juntadeandalucia.es>
2022-07-27 13:26:43 +0200

    typescript port release

--- virtual-battery-proxima-energia.js
+++ virtual-battery-proxima-energia.js
@@ -8,7 +8,7 @@ module.exports = function(RED) {
         var nodeBattery=new NodeBatteryProxima.NodeBatteryProxima(this,config);
 
         nodeBattery._readFromContext();
-        
+
         nodeBattery.afterSend=function(msg){
             console.log({sended:msg});
         };
@@ -20,25 +20,6 @@ module.exports = function(RED) {
         this.on('input',(msg,send,done)=>nodeBattery.onInput(msg,send,done));
 
      
-    }
-
-
-
-
-
- /*   function _readFromContext(nodeContext){
-        let recoveredBatBalance=nodeContext.get("payLoadBatteryBalance");
-        let batteryBalance=null;
-        if(recoveredBatBalance !== undefined){
-            let batBal=JSON.parse(recoveredBatBalance);
-            batteryBalance=new BatteryBalance(batBal.energyImported,batBal.energyFeeded,batBal.energyLossed,batBal.batteryLoad);
-        }else{
-            batteryBalance=new BatteryBalance(0,0,0,0);
-        }
-        return batteryBalance;
-    }
-    */
-
-    
+    }    
     RED.nodes.registerType("virtual-battery-proxima-energia",VirtualBatteryProximaEnergiaNode);
 }
